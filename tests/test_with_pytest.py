c5f47252e1c4e4a509b5b9c586be31234b7a0d77
Sudharsanan <sudharsans762@gmail.com>
2024-03-25 08:56:32 +0530

    this is commited by third person

--- tests/test_with_pytest.py
+++ tests/test_with_pytest.py
@@ -10,7 +10,9 @@ def test_firstProgramming():
 @pytest.mark.smoke
 def test_secondGreekCreditCard():
     print("Good Morning")
+    print("good Afternoon")
 
 
 def test_secondFix(setup):
     print("I will be executed at the second")
+    print("Because I am lazy")
